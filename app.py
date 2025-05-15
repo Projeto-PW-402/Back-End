9c8658c368f34746a76620a008165500827e534c
notmyfault <fallenangelhd06@gmail.com>
2025-05-15 21:06:34 +0100

    Material API

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 class User:
     def __init__(self):
         self.id = 0
-        self.name = ""
+        self.nome = ""
         self.morada = ""
         self.dataNascimento = ""
         self.especialidade = ""
@@ -14,7 +14,31 @@ class User:
         self.listaAuditorias = ""
 
 
-id = 0
+class Material:
+    def __init__(self):
+        self.id = 0
+        self.nome = ""
+        self.tipo = ""
+        self.descricao = ""
+        self.quant = ""
+        self.preco = ""
+
+# Fazer funcao para ver o ultimo id no array
+user_id = 0
+material_id = 0
+
+def get_last_user_id():
+    global user_id
+    if len(userList) > 0:
+        user_id = userList[-1]['id'] + 1
+    else:
+        user_id = 0
+def get_last_material_id():
+    global material_id
+    if len(materialList) > 0:
+        material_id = materialList[-1]['id'] + 1
+    else:
+        material_id = 0
 
 userList = []
 materialList = []
@@ -27,14 +51,14 @@ def home():
 
 @app.route('/addUser', methods=['POST'])
 def add_user():
-    global id
+    global user_id
     data = request.get_json()
     user = User()
     if data == None:
         return jsonify({"error": "No data provided"}), 400
 
-    user.id = id
-    user.name = data.get('name')
+    user.id = user_id
+    user.nome = data.get('name')
     user.morada = data.get('morada')    
     user.dataNascimento = data.get('dataNascimento')
     user.especialidade = data.get('especialidade')
@@ -44,10 +68,9 @@ def add_user():
 
     userList.append(user.__dict__)
 
-    id += 1
+    user_id += 1
 
-    # Salva os dados no ficheiro JSON
-    saveData()
+    saveUserData()
 
     return jsonify({"message": "User added successfully", "user": user.__dict__}), 201
 
@@ -66,7 +89,46 @@ def get_user(user_id):
             return jsonify(user), 200
     return jsonify({"message": "User not found"}), 404
 
-def loadData():
+@app.route('/addMaterial', methods=['POST'])
+def add_material():
+    global material_id
+    data = request.get_json()
+    material = Material()
+    if data == None:
+        return jsonify({"error": "No data provided"}), 400
+
+    material.id = material_id
+    material.nome = data.get('name')
+    material.tipo = data.get('tipo')
+    material.descricao = data.get('descricao')
+    material.quant = data.get('quant')
+    material.preco = data.get('preco')
+
+    materialList.append(material.__dict__)
+
+    material_id += 1
+
+    saveMaterialData()
+
+    return jsonify({"message": "Material added successfully", "user": material.__dict__}), 201
+
+@app.route('/getMaterials', methods=['GET'])
+def get_materials():
+    if len(materialList) == 0:
+        return jsonify({"message": "No materials found"}), 404
+    
+    return jsonify(materialList), 200
+
+@app.route('/getMaterial/<int:material_id>', methods=['GET'])
+def get_material(material_id):
+    global materialList
+    for material in materialList:
+        if material['id'] == material_id:
+            return jsonify(material), 200
+    return jsonify({"message": "Material not found"}), 404
+
+
+def loadUserData():
     global userList
     try:
         with open("users.json", "r", encoding="utf-8") as file:
@@ -75,14 +137,30 @@ def loadData():
         userList = []
     except json.JSONDecodeError:
         userList = []
+def loadMaterialData():
+    global materialList
+    try:
+        with open("materials.json", "r", encoding="utf-8") as file:
+            materialList = json.load(file)
+    except FileNotFoundError:
+        materialList = []
+    except json.JSONDecodeError:
+        materialList = []
 
-def saveData():
+def saveUserData():
     with open("users.json", "w", encoding="utf-8") as file:
+        json.dump(userList, file, indent=4)
+
+    return jsonify({"message": "Data saved successfully"}), 200
+
+def saveMaterialData():
+    with open("materials.json", "w", encoding="utf-8") as file:
         json.dump(userList, file, indent=4)
 
     return jsonify({"message": "Data saved successfully"}), 200
 
 
 if __name__ == '__main__':
-    loadData()
+    loadUserData()
+    loadMaterialData()
     app.run(debug=True)
